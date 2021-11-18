ed5987792ce8bb01f280a2282eba57ea572e33ee
AleksSafonov01 <aleks.safonov01@gmail.com>
2021-11-18 17:58:07 +0300

    update main

--- main.py
+++ main.py
@@ -57,20 +57,21 @@ class PdfGenerator:
         return result_pdf
 
 
-cert_gen = PdfGenerator()
-invite_gen = PdfGenerator('invite.pdf')
-cert2_gen = PdfGenerator('cert3.pdf')
 
+invite_gen = PdfGenerator('invite.pdf')
+cert_gen = PdfGenerator('cert3.pdf')
+cert2_gen = PdfGenerator('cert2.pdf')
 
 @my_tg_client.on_message(filters.command('gen'))
-def gen_cert(app: Client, msg: Message):
+def gen_cert2(app: Client, msg: Message):
     name = msg.text[5:]
 
-    surname, name = name.split(' ', maxsplit=1)
+    surname, first_name, middle_name, email = name.split(' ')
+    name = f'{first_name} {middle_name}'
 
-    pdf = cert_gen.gen_cert_with_name(surname, name, bold=True, font_size=18)
+    pdf = cert2_gen.gen_cert_with_name(surname, name, bold=True, font_size=18)
 
-    result_file_name = f'{surname} {name}.pdf'
+    result_file_name = f'{email}.pdf'
     with open(result_file_name, "wb") as file:
         pdf.write(file)
 
@@ -92,14 +93,29 @@ def gen_invite(app: Client, msg: Message):
     msg.reply_document(result_file_name)
 
 
-@my_tg_client.on_message(filters.command('cert'))
-def gen_c2(app: Client, msg: Message):
+'''@my_tg_client.on_message(filters.command('cert'))
+def gen_(app: Client, msg: Message):
     name = msg.text[6:]
 
     surname, first_name, middle_name, title = name.split(' ', maxsplit=3)
     name = f'{first_name} {middle_name}'
 
-    pdf = cert2_gen.gen_cert_with_name(surname, name, y_text_offset=43, title_offset=5, leading=15, font_size=18, one_line=True, bold=True, title_font_size=13)
+    pdf = cert2_gen.gen_cert_with_name(surname, name, title, y_text_offset=43, title_offset=5, leading=15, font_size=18, one_line=True, bold=True, title_font_size=13)
+
+    result_file_name = f'{surname} {name}.pdf'
+    with open(result_file_name, "wb") as file:
+        pdf.write(file)
+
+    msg.reply_document(result_file_name)'''
+
+
+@my_tg_client.on_message(filters.command('19'))
+def gen_cert(app: Client, msg: Message):
+    name = msg.text.split(' ', maxsplit=1)[1]
+
+    surname, name = name.split(' ', maxsplit=1)
+
+    pdf = cert_gen.gen_cert_with_name(surname, name, y_text_offset=136, title_offset=5, leading=15, font_size=24, one_line=True, bold=True, title_font_size=13)
 
     result_file_name = f'{surname} {name}.pdf'
     with open(result_file_name, "wb") as file:
